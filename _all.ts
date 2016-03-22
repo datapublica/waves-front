93210db7b8801c41dcdbdc397f448de6e76a615f
cdeon <clement.deon@data-publica.com>
2016-03-22 17:54:59 +0100

    Hello world setup

---
+++ _all.ts
@@ -0,0 +1,7 @@
+/// <reference path="typings/main.d.ts" />
+/// <reference path="app/config/core/coreConfig.ts" />
+/// <reference path="app/config/core/coreRun.ts" />
+/// <reference path="app/modules/homepage/index.ts" />
+
+declare var ON_PROD: boolean;
+declare var ON_TEST: boolean;
